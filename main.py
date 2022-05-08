1084c6c5d8e21f9ed79410995cd7c19d4560b1f7
TlM3z <105140178+TlM3z@users.noreply.github.com>
2022-05-08 22:24:15 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from ast import If
+from dis import show_code
+from turtle import done
+import webbrowser
+
+a = "10"
+
+for i in range(int(a)):
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+    webbrowser.open("https://www.facebook.com/", new=10)
+
+if(done):exit
+
+print ("done")
